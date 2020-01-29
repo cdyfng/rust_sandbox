d37a9559cc4c16e9863295e1a7815628003150a5
cdyfng <xiaofeng_chen2007@163.com>
2020-01-30 00:22:46 +0800

    generic add

---
+++ src/generic.rs
@@ -0,0 +1,30 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &U {
+    	&self.y
+    }
+
+}
+
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+
+
+pub fn run() {
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+      println!("{} {} {}", both_float.distance_from_origin() , both_integer.distance_from_origin(), integer_and_float.y());
+}
