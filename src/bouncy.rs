c0a7978a1556dee43df46e0a9c6570d2ebc6799f
cdyfng <xiaofeng_chen2007@163.com>
2020-02-18 20:08:55 +0800

    change bouncy visualization with pancurses

--- src/bouncy.rs
+++ src/bouncy.rs
@@ -1,4 +1,6 @@
-use std::fmt::{Display, Formatter};
+//use std::fmt::{Display, Formatter};
+use pancurses::{Window, Input, initscr, endwin};
+
 enum VertDir {
     Up,
     Down,
@@ -27,10 +29,14 @@ struct Game {
 
 
 impl Game{
-    fn new() -> Game {
+    fn new(window: &Window) -> Result<Game, String> {
+        let (max_y, max_x) = window.get_max_yx();
+        if max_y < 10 || max_x < 10 {
+            return Err(String::from("Window is too small, exiting"));
+        }
         let frame = Frame {
-            width : 60, 
-            height : 30, 
+            width : max_x as u32 - 2, 
+            height : max_y as u32 - 2, 
         };
         let ball = Ball {
             x : 2,
@@ -38,7 +44,7 @@ impl Game{
             vert_dir : VertDir::Up,
             horiz_dir : HorizDir::Left,
         };
-        Game {frame, ball}
+        Ok(Game {frame, ball})
     }
 
     fn step(&mut self) {
@@ -77,46 +83,76 @@ impl Ball {
 }
 
 
-impl Display for Game {
-    fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
-        let top_bottom =  |fmt: &mut Formatter| {
-            write!(fmt, "+");
-            for _ in 0..self.frame.width {
-                write!(fmt, "-");
-            }
-            write!(fmt, "+\n");
-        };
+// impl Display for Game {
+//     fn fmt(&self, fmt: &mut Formatter) -> std::fmt::Result {
+//         let top_bottom =  |fmt: &mut Formatter| {
+//             write!(fmt, "+");
+//             for _ in 0..self.frame.width {
+//                 write!(fmt, "-");
+//             }
+//             write!(fmt, "+\n");
+//         };
 
-        let body = |fmt: &mut Formatter| {
-            for row in 0..self.frame.height {
-                write!(fmt, "|");
-                // more code will go here
-                for column in 0..self.frame.width {
-                    let c = if row == self.ball.y && column == self.ball.x {
-                        'o'
-                    } else {
-                        ' '
-                    };
-                    write!(fmt, "{}", c);
-                }
-                write!(fmt, "|\n");
-            }
-        };
+//         let body = |fmt: &mut Formatter| {
+//             for row in 0..self.frame.height {
+//                 write!(fmt, "|");
+//                 // more code will go here
+//                 for column in 0..self.frame.width {
+//                     let c = if row == self.ball.y && column == self.ball.x {
+//                         'o'
+//                     } else {
+//                         ' '
+//                     };
+//                     write!(fmt, "{}", c);
+//                 }
+//                 write!(fmt, "|\n");
+//             }
+//         };
 
 
-        top_bottom(fmt);
-        body(fmt);
-        top_bottom(fmt);
-        Ok(())
-    }
-}
+//         top_bottom(fmt);
+//         body(fmt);
+//         top_bottom(fmt);
+//         Ok(())
+//     }
+// }
 
-pub fn run(){
-    let mut game = Game::new();
-    let sleep_duration = std::time::Duration::from_millis(33);
+pub fn run()-> Result<(), String> {
+    let window = initscr();
+    window.timeout(33);
+
+    let mut game = Game::new(&window)?;
     loop{
-        println!("{}", game);
-        game.step();
-        std::thread::sleep(sleep_duration);
+        window.clear();
+        window.border(
+            '|', // left
+            '|', // right
+            '-', // top
+            '-', // bottom
+            '+', // top left
+            '+', // top right
+            '+', // bottom left
+            '+', // bottom right           
+        );
+        window.mvaddch(game.ball.y as i32 + 1, game.ball.x as i32 + 1, 'o');
+        window.mv(0, 0);
+
+        window.refresh();
+        match window.getch(){
+            Some(Input::Character('q')) => {
+                endwin();
+                println!("Thanks for playing!");
+                return Ok(());   
+            }
+            _ => {
+                game.step();
+            }    
+        }
     }
+    //let sleep_duration = std::time::Duration::from_millis(33);
+    // loop{
+    //     println!("{}", game);
+    //     game.step();
+    //     std::thread::sleep(sleep_duration);
+    // }
 }
